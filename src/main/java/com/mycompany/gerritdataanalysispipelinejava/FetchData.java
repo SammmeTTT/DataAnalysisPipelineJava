d1b811626de952d32b27244183835fab48b74e1e
SammmeTTT <samme.technics@gmail.com>
2023-02-27 16:46:01 +0100

    Updated version of code

--- src/main/java/com/mycompany/gerritdataanalysispipelinejava/FetchData.java
+++ src/main/java/com/mycompany/gerritdataanalysispipelinejava/FetchData.java
@@ -6,7 +6,11 @@ package com.mycompany.gerritdataanalysispipelinejava;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.mycompany.gerritdataanalysispipelinejava.JsonContent;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import static com.mycompany.gerritdataanalysispipelinejava.AnalyseData.analyzeJsonData;
+import java.lang.Object;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import static java.lang.Runtime.Version.parse;
@@ -24,73 +28,133 @@ import org.json.simple.JSONObject;
 import static org.json.simple.JSONValue.parse;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import java.net.URI;
+import java.net.http.HttpHeaders;
+import java.util.Optional;
+import javax.ws.rs.core.UriBuilder;
 
-/**
- *
- * @author samme
- */
 public class FetchData {
-    
 
-    //trying to accessJson file
-    public static void accessJsonFile(){
-        String path = System.getProperty("." + File.separator + "output.json");
-        System.out.println("Successfully get file: " + path);
+    //trying to access Json file
+    public static JSONArray accessJsonFile() throws FileNotFoundException, IOException, ParseException{//file reader here
+        FileReader readJsonFile;
+        Object obj;
+        JSONArray jsonObjArray = null;
+        try {
+            JSONParser parser = new JSONParser();
+            readJsonFile = new FileReader("output.json");
+            obj = parser.parse(readJsonFile);
+            jsonObjArray = (JSONArray) obj;
+        }
+        
+        catch(IOException | ParseException e){
+           System.out.println("Couldn't access the json file");
+        }
+        return jsonObjArray;
     }
     
     
     
     //write jsondata to a json file
-    public static void writeJsonToFile(JSONObject jsonObject) throws IOException{
-        try (FileWriter jsonFile = new FileWriter("output.json")) {//GerritDataAnalysisPipelineJava/scr/main/java/com/mycompany/gerritdataanalysispipelinejava/
-            jsonFile.write(jsonObject.toJSONString());
-            jsonFile.close();
+    public static void writeJsonToFile(JSONArray jsonArray) throws IOException{
+        try (FileWriter writeToJson = new FileWriter("output.json")) {//GerritDataAnalysisPipelineJava/scr/main/java/com/mycompany/gerritdataanalysispipelinejava/
+            if(jsonArray != null){
+                writeToJson.write(jsonArray.toJSONString());
+                writeToJson.close();
+            }else{
+                System.out.println("There are no data to write to the json file, look at the API request and see whats wrong and if you got nay data");
+            }
         }
         catch(IOException e){
             System.out.println("Couldn't write json string to json file");
         }
-        accessJsonFile();
     }
     
     
     //sends api request and get a string response
-    public static String apiRequest() throws IOException, InterruptedException {
-        String requestUrl = "https://android-review.googlesource.com/changes/";
+    public static String apiRequest() throws IOException, InterruptedException, ParseException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write a start date in format: 2023-02-13");
+        String startDate = "{"+ scanner.next() +"}";
+        System.out.println("Write a end date in format: 2023-02-15");
+        String endDate ="{" + scanner.next() +"}";
+        
+        //String requestUrl = "https://android-review.googlesource.com/changes/?q=after:"+startDate1 + "ANDbefore:"+ endDate1 + "&status:open";
+               // "https://android-review.googlesource.com/changes/?q=status:open,5000"; //?q=after:{2023-02-13}ANDbefore:{2023-02-14}"; //"https://android-review.googlesource.com/changes/?q=status:open"; //"https://android-review.googlesource.com/changes/?q=after:%7B" + startDate + "%7DANDbefore:%7B" + endDate + "%7D";
+                //"https://android-review.googlesource.com/changes/"; //?q=status:open";
+                //"https://android-review.googlesource.com/changes/?q=after:" + "%7B2023-02-13%7D" + "ANDbefore:" + "%7B2023-02-14%7D";
+                //"https://android-review.googlesource.com/changes/?q=after:%7B" + startDate + "%7DANDbefore:%7B" + endDate + "%7D";
+                //https://android-review.googlesource.com/changes/?q=after:{2023-02-13}ANDbefore:{2023-02-14}ANDstatus:open
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
+        String jsonData; 
+        String jsonResponseData = "";
+        boolean sendRequest = true;
+        HttpResponse<String> response = null;
+        JSONParser parseToJson = new JSONParser();
+        JSONArray arrayToParse;
+        JSONArray singleArray = null;
+        
+        try{
+            URI url = UriBuilder.fromUri("https://android-review.googlesource.com/changes/?q=status:open")
+                .queryParam("q", "status:closed")
+                .queryParam("q", "after:{2023-02-13}ANDbefore:{2023-02-14}")
+                //.queryParam("q", "skip-visibility:true")
+                .build(startDate, endDate);
+            
+            System.out.println("url: " + url + "\n");
+            
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
                 .GET()
                 .header("accept", "application/json")
-                .uri(URI.create(requestUrl))
+                .uri(url)
                 .build();
-
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            
+            //while(sendRequest==true){
+                response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                jsonData = response.body();
+                jsonResponseData = jsonData.substring(5, jsonData.length()-1);
+//                arrayToParse = (JSONArray) parseToJson.parse(jsonResponseData);//skapar 1 obj inuti två arrayer?? ist. för ett det första är tomt
+//                singleArray = (JSONArray) arrayToParse.get(0);
+//                JSONObject lastObj = (JSONObject) singleArray.get(0);
+//                sendRequest = (boolean) lastObj.get("_more_changes");
+            //}
         
-        String jsonData = response.body();
-        jsonData = jsonData.substring(4, jsonData.length()-1);
-        //jsonData = gson.fronJson(response.body(), JsonContent.class); // import gson
-        
-        //json string to json objects    
-        //ObjectMapper mapper = new ObjectMapper();
-        //List<JsonContent> jsonContent = mapper.readValue(jsonData, new TypeReference < List < JsonContent >> ()  {});
-        //jsonContent.forEach(x -> System.out.println(x));
-        return jsonData;
+       
+        }
+        catch(IOException |IllegalArgumentException |InterruptedException e){
+            System.out.println("Something is wrong with the request url, please change it and try again!");
+        }
+       
+        return jsonResponseData;
     }
 
     //makes an jsonobject of the request string
-    static JSONObject toJsonObject(String informationString) throws ParseException {
+    static JSONArray toJsonArray(String informationString) throws ParseException {
+        System.out.println("request string: " + informationString);
         JSONParser parseToJson = new JSONParser();
-        JSONArray objectToParse = (JSONArray) parseToJson.parse(String.valueOf(informationString));
-        JSONObject jsonData = (JSONObject) objectToParse.get(0);
-        return jsonData;
-        //System.out.println(countryData.get("_number"));
+        JSONArray arrayToParse;
+        JSONArray singleArray = null;
+        
+        try{
+            arrayToParse = (JSONArray) parseToJson.parse(String.valueOf(informationString));//skapar 1 obj inuti två arrayer?? ist. för ett det första är tomt
+            singleArray = (JSONArray) arrayToParse.get(0);
+        }
+        catch(ParseException e){
+            System.out.println("Could not parse string to json Array");
+        }
+        return singleArray;
     }
 
     public static void main(String[] args) throws IOException, InterruptedException, ParseException {
+        
         String client = apiRequest();
-        JSONObject object = toJsonObject(client);
-        writeJsonToFile(object);
-
+        JSONArray array = toJsonArray(client);
+        //System.out.println("Size: " + array.size());
+        
+        writeJsonToFile(array);
+        JSONArray jsonData = accessJsonFile();
+        analyzeJsonData(jsonData);
     }
 
 }
--- src/main/java/com/mycompany/gerritdataanalysispipelinejava/VisualizeData.java
+++ src/main/java/com/mycompany/gerritdataanalysispipelinejava/VisualizeData.java
@@ -4,10 +4,44 @@
  */
 package com.mycompany.gerritdataanalysispipelinejava;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.FileOutputStream;
+import javax.swing.JFileChooser;
+
 /**
  *
  * @author samme
  */
 public class VisualizeData {
+    public static void createPDF(){
+        JFileChooser pdfFile = new JFileChooser();
+        pdfFile.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        
+        int saveStatus = pdfFile.showSaveDialog(pdfFile);
+        String path = "";
+    
+        if(saveStatus == JFileChooser.APPROVE_OPTION){
+            path = pdfFile.getSelectedFile().getPath();
+        }
+        
+        Document doc = new Document();
+        try{
+            PdfWriter.getInstance(doc, new FileOutputStream(path+"gerritDataVisualization.pdf"));
+            doc.open();
+            
+            //Pdf pdfTable = new PdfTable(3); // ev vanlig table istället
+            
+            
+            
+            
+        }
+        catch(Exception e){
+            System.out.println("");
+        }
+        
+    }
+    
+    
     
 }
